c63ae317bb37d31f77b7dc1eae729c68d37cacdb
Lukasz Dobrowolski <l00k+github@protonmail.com>
2021-01-23 05:30:15 +0100

    Add validate.js

--- Validator/ValidateJsExt.ts
+++ Validator/ValidateJsExt.ts
@@ -1,5 +1,33 @@
 import ValidateJsExt from 'validate.js';
+import { isset } from '../Utility';
 
+ValidateJsExt.formatters.intiv = function(errors : any[]) {
+    let result : MethodValidationErrors = {};
+    errors.map((error) => {
+        if (!isset(() => result[error.attribute])) {
+            result[error.attribute] = [];
+        }
+
+        result[error.attribute].push({
+            rule: error.validator,
+            options: error.options,
+        });
+    });
+    return result;
+};
+
+ValidateJsExt.validators.type.types.numeric = function(value : any) {
+    return value == Number(value);
+};
+
+
+type PropertyValidationErrors = {
+    rule : string,
+    options? : any[],
+}[];
+type MethodValidationErrors = {
+    [field : string] : PropertyValidationErrors
+};
 
 type PropertyValidationErrors = {
     rule : string,
