873455e3b102a20632fd243b50e1a16c4da97c4d
Lukasz Dobrowolski <l00k+github@protonmail.com>
2021-02-01 11:50:19 +0100

    Validator improvement

---
+++ Validator/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ValidationException = void 0;
+const ValidationException_1 = __importDefault(require("./ValidationException"));
+exports.ValidationException = ValidationException_1.default;
